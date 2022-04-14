a5f02020b7ce773f3b3a32774b36e4da950c7109
amitmungare <amungare27@gmail.com>
2022-04-14 17:01:44 +0530

    subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,43 @@ import {data} from '../data'
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-            <div className="tab">Movies</div>
-            <div className="tab">Favorites</div>
-        </div>
+class App extends React.Component{
 
-        <div className="list">
-          {data.map(movie => (
-            <MovieCard movie={movie}/>
-          ))}
+  componentDidMount(){
+    const {store } = this.props;
+
+    store.subscribe(() =>{
+      this.forceUpdate();
+    })
+    store.dispatch({
+      type:'ADD_MOVIES',
+      movies: data
+    });
+
+  }
+
+  render() {
+    
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+              <div className="tab">Movies</div>
+              <div className="tab">Favorites</div>
+          </div>
+  
+          <div className="list">
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import movies from './reducers'
+
+const store  = createStore(movies);
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies:[{name: 'Superman'}]
+// });
+
+// console.log(store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
